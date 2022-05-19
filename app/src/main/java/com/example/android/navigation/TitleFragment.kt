e8c4a2964c1fd3518843f62884413adb5b874ca6
Camilo <jcam.qvision@gmail.com>
2022-05-18 20:19:03 -0500

    fragments added to the navigation graph

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 class TitleFragment : Fragment() {
@@ -15,6 +16,11 @@ class TitleFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater,
         R.layout.fragment_title, container, false)
+
+        binding.playButton.setOnClickListener{
+            view: View -> view.findNavController().navigate(R.id.action_titleFragmentN_to_gameFragment)
+        }
+
         return binding.root
     }
 
